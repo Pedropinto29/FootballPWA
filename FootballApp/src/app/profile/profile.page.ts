095f8a1a63c9d56285d482491bd1271ec8f29cd4
Pedropinto29 <pedrodacostapinto@gmail.com>
2023-06-07 10:44:32 +0200

    clean up + calendar css

--- FootballApp/src/app/profile/profile.page.ts
+++ FootballApp/src/app/profile/profile.page.ts
@@ -18,9 +18,24 @@ export class ProfilePage {
     )
   }
 
+  generatePlace(){
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const number1 = "012"
+    const number2 = "0123456789"
+    let result =''
+
+    const gateLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
+    result += gateLetter
+    const gateNumber1 = number1[Math.floor(Math.random() * number1.length)]
+    result += gateNumber1
+    const gateNumber2 = number2[Math.floor(Math.random() * number2.length)]
+    result += gateNumber2
+    return result
+  }
+
   annualPlace() {
     console.log(this.user.userId)
-    this.user.annualPlace = "Gate E"
+    this.user.annualPlace = this.generatePlace()
     this.userService.annualPlace(this.user.userId, this.user).subscribe(
       data => {
         console.log("Bought Annual Place")
